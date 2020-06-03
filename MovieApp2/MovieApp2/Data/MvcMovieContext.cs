e7328563c7ad70f6b145687bf2df32a2e8bc1e25
Guilherme Canalli da Silva <canalg@alcoa.com>
2020-06-03 17:06:41 -0300

    Created SQL Database and Controllers

---
+++ MovieApp2/MovieApp2/Data/MvcMovieContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MovieApp2.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MovieApp2.Data
+{
+    public class MvcMovieContext : DbContext
+    {
+        public MvcMovieContext(DbContextOptions<MvcMovieContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Movie> Movie { get; set; }
+    }
+}
---
+++ MvcMovie/MvcMovie/Startup.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Owin;
+using Owin;
+using MvcMovie.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using System.Configuration;
+
+[assembly: OwinStartup(typeof(MvcMovie.Startup))]
+
+namespace MvcMovie
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddDbContext<MvcMovieContext>(options =>
+                options.UseSqlServer(ConfigurationManager.AppSettings["MvcMovieContext"]));
+        }
+
+        public void Configuration(IAppBuilder app)
+        {
+            // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=316888
+        }
+    }
+}
